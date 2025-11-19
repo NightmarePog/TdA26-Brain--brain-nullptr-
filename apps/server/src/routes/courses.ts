508d522ec88e5ec44be6dbaef00b89641f145888
Ari <109524755+Pleushy@users.noreply.github.com>
2025-11-19 14:35:04 +0100

    feat: add course update endpoint

--- apps/server/src/routes/courses.ts
+++ apps/server/src/routes/courses.ts
@@ -17,9 +17,9 @@ courseRoutes.get("/", async (_req, res) => {
 
 courseRoutes.post("/", async (req, res) => {
     try {
-		const uuid = randomUUID();
-        const name = req.body.name;
-        const desc = req.body.description || '';
+		const uuid : string = randomUUID();
+        const name : string = req.body.name;
+        const desc : string = req.body.description || '';
 		await pool.execute(`
             INSERT INTO courses (uuid, name, description)
             VALUES (?, ?, ?)
@@ -34,13 +34,47 @@ courseRoutes.post("/", async (req, res) => {
 
 courseRoutes.get("/:uuid", async (req, res) => {
 	try {
-		const uuid = req.params.uuid;
-		const [course] = await pool.execute(`
-			SELECT * FROM courses WHERE uuid = ?
-		`,[uuid]);
-		course.length > 0 ? res.status(200).json(course) : res.status(404).json({ message: "Invalid course uuid" });
+		const uuid : string = req.params.uuid;
+		const course = await findCourseByUUID(uuid);
+		course ? res.status(200).json(course) : res.status(404).json({ message: "Invalid course uuid" });
 	} catch (error) {
 		console.error("Error getting course details:", error);
 		res.status(500).json({ error: "Failed to get course details" });
 	}
 });
+
+courseRoutes.put("/:uuid", async (req, res) => {
+	try {
+		const uuid : string = req.params.uuid;
+
+		const course = await findCourseByUUID(uuid);
+		if (!course) {
+			res.status(404).json({ message: "Invalid course uuid" });
+			return;
+		}
+
+		if (req.body) {
+			const name : string = req.body.name != null ? req.body.name : course.name;
+			const desc : string = req.body.description != null ? req.body.description : course.description;
+	
+			await pool.execute(`
+				UPDATE courses
+				SET name = ?, description = ?
+				WHERE uuid = ?
+			`,[name, desc, uuid]);
+		}
+
+		res.status(200).json(await findCourseByUUID(uuid));
+		
+	} catch (error) {
+		console.error("Error getting course details:", error);
+		res.status(500).json({ error: "Failed to get course details" });
+	}
+});
+
+async function findCourseByUUID(uuid : string) {
+	const [courses] = await pool.execute(`
+			SELECT * FROM courses WHERE uuid = ?
+	`,[uuid]);
+	return courses.length == 1 ? courses[0] : null;
+};
