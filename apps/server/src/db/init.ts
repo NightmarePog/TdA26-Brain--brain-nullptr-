f3d110f168add1e6c2bfcde9076756a2f74353c0
Ari <109524755+Pleushy@users.noreply.github.com>
2025-12-05 20:40:52 +0100

    refactor(backend): remove test data

--- apps/server/src/db/init.ts
+++ apps/server/src/db/init.ts
@@ -55,6 +55,8 @@ export async function initDatabase() {
 				CREATE TABLE IF NOT EXISTS files (
 					uuid CHAR(36) PRIMARY KEY,
 					name VARCHAR(255) NOT NULL,
+					type VARCHAR(255) NOT NULL,
+					fileUrl VARCHAR(1000) NOT NULL,
 					description VARCHAR(1000),
 					mimeType VARCHAR(255),
 					sizeBytes INT,
@@ -67,6 +69,7 @@ export async function initDatabase() {
 				CREATE TABLE IF NOT EXISTS urls (
 					uuid CHAR(36) PRIMARY KEY,
 					name VARCHAR(255) NOT NULL,
+					type VARCHAR(255) NOT NULL,
 					url VARCHAR(1000) NOT NULL,
 					faviconUrl VARCHAR(1000),
 					description VARCHAR(1000),
@@ -74,33 +77,7 @@ export async function initDatabase() {
 				)
 			`);
 			console.log("urls OK");
-
-			console.log("\nInserting test data...");
-
-			/** Testing data */
-			await pool.execute(`
-				INSERT IGNORE INTO courses (uuid, name, description)
-				VALUES ('aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee', 'This is a name!', 'This is a description!')
-			`);
-			console.log("course1 OK");
-
-			await pool.execute(`
-				INSERT IGNORE INTO materials (uuid, course_uuid)
-				VALUES ('12345678-bbbb-1234-4321-eeeeeeeeeeee', 'aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee')
-			`);
-			console.log("material OK");
-
-			await pool.execute(`
-				INSERT IGNORE INTO files (uuid, type, name)
-				VALUES ('12345678-bbbb-1234-4321-eeeeeeeeeeee', 'This is a file type!', 'This is a name of a file!')
-			`);
-			console.log("file OK");
 			
-			await pool.execute(`
-				INSERT IGNORE INTO courses (uuid, name, description)
-				VALUES ('abcdefgh-ijkl-mnop-qrst-uvwxyzabcdef', 'Jetset life is gonna kill you', 'Gaze into her killing jar id sometimes stare for hours, she even poked the holes so i can breathe. She bought the last line, im just the worst kind of guy to argue with what you might find and for the last night i lie, could i lie with you? ALRIGHT, GIVE UP, GET DOWN, Its just the hardest part of living!')
-			`);
-			console.log("course2 OK");
 			break;
 
 		} catch (error) {
