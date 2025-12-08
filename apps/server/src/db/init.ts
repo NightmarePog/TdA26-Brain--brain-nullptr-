cab9e6817174ddb94e43c0e7e0ee618539faffec
Ari <109524755+Pleushy@users.noreply.github.com>
2025-12-08 16:58:10 +0100

    feat(backend): add quizzes

--- apps/server/src/db/init.ts
+++ apps/server/src/db/init.ts
@@ -85,20 +85,60 @@ export async function initDatabase() {
 				)
 			`);
 			console.log("urls OK");
+
+			await pool.execute(`
+				CREATE TABLE IF NOT EXISTS quizzes (
+					uuid CHAR(36) PRIMARY KEY,
+					course_uuid CHAR(36) NOT NULL,
+					title VARCHAR(255) NOT NULL,
+					attemptsCount INT NOT NULL,
+					created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+					updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+					update_count INT DEFAULT 0,
+					FOREIGN KEY (course_uuid) REFERENCES courses(uuid) ON DELETE CASCADE
+				)
+			`);
+			console.log("quizzes OK");
+
+			await pool.execute(`
+				CREATE TABLE IF NOT EXISTS questions (
+					uuid CHAR(36) PRIMARY KEY,
+					quiz_uuid CHAR(36) NOT NULL,
+					type VARCHAR(255) NOT NULL,
+					question VARCHAR(1000) NOT NULL,
+					created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+					updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+					update_count INT DEFAULT 0,
+					FOREIGN KEY (quiz_uuid) REFERENCES quizzes(uuid) ON DELETE CASCADE
+				)
+			`);
+			console.log("questions OK");
+
+			await pool.execute(`
+				CREATE TABLE IF NOT EXISTS options (
+					uuid CHAR(36) PRIMARY KEY,
+					question_uuid CHAR(36) NOT NULL,
+					idx INT NOT NULL,
+					opt VARCHAR(1000) NOT NULL,
+					correct BOOL NOT NULL,
+					FOREIGN KEY (question_uuid) REFERENCES questions(uuid) ON DELETE CASCADE
+				)
+			`);
+			console.log("options OK");
 			
 			console.log("\nCreating users..");
 
 			await pool.execute(`
 				INSERT IGNORE INTO users (email, name, password, admin)
 				VALUES (?, ?, ?, ?)
-			`, ["lecturer@email.com", "lecturer", await sha512("TdA26!"), true]);
-			console.log("admin user OK")
+			`, ["lecturer@email.com", "lecturer", await sha512(`TdA26!${process.env.PASSWORD_SALT}`), true]);
+			console.log("admin user OK");
 
 			await pool.execute(`
 				INSERT IGNORE INTO users (email, name, password, admin)
 				VALUES (?, ?, ?, ?)
 			`, ["student@email.com", "student", await sha512("1234"), false]);
-			console.log("student user OK")
+			console.log("student user OK");
 
 			break;
 
