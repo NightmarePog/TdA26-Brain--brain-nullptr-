57ee9f5f00600ff0b2bb04fe4bab93af5e20a70f
Nightmare Pog <44711407+NightmarePog@users.noreply.github.com>
2026-03-06 22:19:02 +0100

    Update next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -20,6 +20,22 @@ const nextConfig: NextConfig = {
         hostname: "**.tourde.app",
         pathname: "/api/courses/**",
       },
+      {
+        protocol: "https",
+        hostname: process.env.NEXT_PUBLIC_API_URL || "localhost",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: process.env.NEXT_PUBLIC_API_URL || "localhost",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+        pathname: "/api/courses/**",
+      },
     ],
     domains: ["www.google.com"],
   },
